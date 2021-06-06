ec93de99e05fcacb0cd1a501e1ea9b2e5d92b127
Lutfullah <lutfullahclnk1104@gmail.com>
2021-06-06 18:46:15 +0300

    For Loops

---
+++ 10)For Loops/script.js
@@ -0,0 +1,29 @@
+
+for(var i=0; i<10; i++) {
+    document.write(i+"<br>");
+}
+
+//ilk kısmı değer ikinci kısmı şart üçüncü kısmı sayaç
+// ilk değerine bakar sonra şartı sorgular sonra fonksiyonu çalıştırır, sonra sayacı artırır.
+
+
+for(var i=10; i>0; i--) {
+    document.write(i+"<hr>")
+}
+
+var i=7;
+for( ;i>0;i-=2) {
+    document.write(i+"<br>")
+}
+
+document.write(i+"<hr>")
+
+
+var i=7;
+for( ;i>0;) {
+    document.write(i+"<br>")
+    i-=2
+}
+
+document.write(i)
+
