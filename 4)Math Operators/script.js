c2be74ffea1adbccfd7fd7b470dbb3261aa36bda
Lutfullah <lutfullahclnk1104@gmail.com>
2021-06-06 18:44:25 +0300

    Math Operators

---
+++ 4)Math Operators/script.js
@@ -0,0 +1,33 @@
+//document.write(10+20);
+//document.write(10-20);
+
+var x=5, y=20 ,sonuc=0;
+sonuc= x+y
+
+document.write(sonuc+"<hr>")
+
+sonuc++;  // sonuc= sonuc+1
+document.write(sonuc+"<br>")
+
+var x=5,y;
+
+y= x++ //post increment  y=5 x=6
+document.write(y+"<br>");
+
+y= ++x  //pre increment  y=7 x=7
+document.write(y)
+document.write("<br>"+x+"<hr>")
+
+
+var a=10,b;
+
+b= a--;
+document.write(a+" "+b+"<br>")
+
+b=--a;
+document.write(a+" "+b+"<hr>");
+
+
+
+var a=8, b=3;
+document.write(a%b)
