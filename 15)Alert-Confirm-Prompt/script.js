4fe9da4c4846c9e2e7b03ab4592ed84005b37f93
Lutfullah <lutfullahclnk1104@gmail.com>
2021-06-06 18:47:46 +0300

    Alert-Prompt-Confirm

---
+++ 15)Alert-Confirm-Prompt/script.js
@@ -0,0 +1,12 @@
+/* var kullanıcıgirişi = prompt("Lütfen yaşınızı giriniz. ")
+
+alert("Doğum yılınız..."+ (2021-kullanıcıgirişi)) */
+
+var giriş = prompt("Devam etmek istiyor musunuz?")
+
+if (giriş=="evet") {
+    document.write("Devam ettiniz...")
+}
+else{
+    document.write("Program bitirildi...")
+}
