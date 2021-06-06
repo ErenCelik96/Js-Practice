73702f19c59352885df2e62f43b844188381d322
Lutfullah <lutfullahclnk1104@gmail.com>
2021-06-06 18:44:07 +0300

    Variables-2

---
+++ 3)Variables-2/script.js
@@ -0,0 +1,15 @@
+//var sayı=5;
+//document.write(sayı+" "+sayı+3);
+//console.log(sayı+sayı);
+
+var sayı=5;
+var isim= "Lütfü Academy"
+var kesirlisayı=5.6;
+var anahtar=true;
+
+document.write(sayı+"<br>"+isim+ "<br>"+kesirlisayı+"<br>"+anahtar);
+console.log(sayı+"\n"+isim+ "\n"+kesirlisayı+"\n"+anahtar)
+
+//document.write fonksiyonu bir html gibi davranır.
+//console.log fonksiyonu bir programlama dili gibi çalışır.
+
