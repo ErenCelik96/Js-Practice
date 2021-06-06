c64aa3d49b5b766bd41a1aac87cbc2c3ef5085ea
Lutfullah <lutfullahclnk1104@gmail.com>
2021-06-06 18:45:40 +0300

    If-Else Controls

---
+++ 8)If-Else Controls/script.js
@@ -0,0 +1,19 @@
+var konu=1;
+
+if( konu==1 ) {
+    document.write("<h1>Matematik</h1>"+"<hr>");
+}
+
+var konu=3;
+
+if( konu==1 ) {
+    document.write("<h1>Matematik</h1>");
+}
+
+else if( konu==2 ) {
+    document.write("<h1>Fizik</h1>")
+}
+
+else {
+    alert("Bir tarafınızdan element uydurmayın.")
+}
