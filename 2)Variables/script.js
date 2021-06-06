773e76722aa45fde4d4b3b780396d9507939e4de
Lutfullah <lutfullahclnk1104@gmail.com>
2021-06-06 18:43:23 +0300

    Variables

---
+++ 2)Variables/script.js
@@ -0,0 +1,12 @@
+// var x=100
+// document.write(x);
+// x++;
+// document.write("<br>"+x)
+
+var x,y,z;
+x = "Lütfü Academy";
+y = "Galatasaray"
+z = x+"<br>"+y;
+
+document.write("<hr>"+z)
+console.log(z)
