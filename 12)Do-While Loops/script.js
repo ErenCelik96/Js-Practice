24bb501a5962057e7f3473ec59b23a1c4fb78561
Lutfullah <lutfullahclnk1104@gmail.com>
2021-06-06 18:46:56 +0300

    Do-While Loops

---
+++ 12)Do-While Loops/script.js
@@ -0,0 +1,11 @@
+var sayı=20;
+
+do{
+    document.write(sayı+"<br>");
+    sayı--;
+}
+while (sayı>0) 
+
+
+// Do-While ile While arasındaki fark: her ne olursa olsun do'nun içine bir kez girer. Ama şartı sağlamazsa devam etmez.
+// While döngüsünde şartı sağlamıyorsa hiç girmez
