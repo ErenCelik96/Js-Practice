1cac1e55d31e326f16b750458a8854fc1b9d437e
Lutfullah <lutfullahclnk1104@gmail.com>
2021-06-06 18:48:48 +0300

    Math Functions

---
+++ 19)Math Functions/script.js
@@ -0,0 +1,25 @@
+/* var kullanıcıgirisi = prompt("Lütfen bir sayı giriniz...");
+
+document.write(Math.abs(kullanıcıgirisi)+"<hr>");
+
+document.write(Math.floor(kullanıcıgirisi)+"<hr>");
+
+document.write(Math.ceil(kullanıcıgirisi)+"<hr>");
+
+document.write(Math.round(kullanıcıgirisi)+"<hr>");
+
+document.write(Math.pow(kullanıcıgirisi,2)+"<hr>"); */
+
+
+document.write(Math.max(1,3,5,7,2,4,6)+"<hr>");
+
+document.write(Math.min(1,3,5,7,2,4,6)+"<hr>");
+
+document.write(Math.ceil(10*Math.random())+"<hr>");
+
+
+
+
+
+
+
