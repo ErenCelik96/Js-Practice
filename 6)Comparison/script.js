cda5bfb8deb0d6e3554c864a160118a43774056a
Lutfullah <lutfullahclnk1104@gmail.com>
2021-06-06 18:45:03 +0300

    Comparison

---
+++ 6)Comparison/script.js
@@ -0,0 +1,18 @@
+//var x=true;
+//document.write(x)
+
+var anahtar= 10<5;
+
+anahtar= 10==10;
+anahtar= 10=="10";  // == değeri içeriğine bakar.
+anahtar = 10==="10"; //=== değeri type kontrolü yapar
+anahtar = 10 ==10.0; //number ile float eşittir.
+
+anahtar= 10 != 11;
+anahtar= 10 !== "10";
+
+anahtar= 10>= 10;
+anahtar= 5<= 4;
+
+document.write(anahtar);
+
