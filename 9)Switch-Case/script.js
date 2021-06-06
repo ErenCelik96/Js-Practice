927eff1899397d2c5947c5690570d5791e845d17
Lutfullah <lutfullahclnk1104@gmail.com>
2021-06-06 18:45:57 +0300

    Switch-Case

---
+++ 9)Switch-Case/script.js
@@ -0,0 +1,14 @@
+var color="red"
+
+switch(color) {
+
+    case "red": document.write("<div style=\"background:red;\">Kırmızı</div>")
+    break;
+    case "yellow": document.write("SARI");
+    break;
+    case "purple" : document.write("MOR");
+    break;
+
+    default:
+        document.write("Böyle bir renk yoktur.")
+}
