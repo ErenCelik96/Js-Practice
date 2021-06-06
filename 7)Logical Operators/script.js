b507d7f0bb55d2c8ee9fa36f00d19f7022d01aa1
Lutfullah <lutfullahclnk1104@gmail.com>
2021-06-06 18:45:21 +0300

    Logical Operators

---
+++ 7)Logical Operators/script.js
@@ -0,0 +1,16 @@
+var anahtar = (10>4) && (5<6); //&& mantıksal olarak ve'ye eşittir.
+
+anahtar= (10>4) || (5>7);  // || mantıksal olarak veya'ya eşittir.
+
+document.write(anahtar)
+
+anahtar = !anahtar  // ünlem işareti Python'daki not değeridir. Tersine çevirir.
+
+document.write("<br>"+anahtar+"<hr>")
+
+
+var yaş=29;
+
+var yetişkinmi= (yaş<18) ? "Yetişkin değildir.":"Yetişkindir.";
+
+document.write(yetişkinmi)
