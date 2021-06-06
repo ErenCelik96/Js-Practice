a50b69c5484e6081b6ed273bcd22d7b1dd43e4f1
Lutfullah <lutfullahclnk1104@gmail.com>
2021-06-06 18:49:12 +0300

    Date Object

---
+++ 20)Date Object/script.js
@@ -0,0 +1,24 @@
+var tarih = new Date()
+
+document.write(tarih+"<hr>")
+
+
+var tarih = new Date("March 5,2014 15:45:45")
+
+document.write(tarih+"<hr>")
+
+var tarih = new Date(93,8,25,18,12,24)
+
+document.write(tarih+"<hr>")
+
+var tarih = new Date()
+
+document.write(tarih.getFullYear()+"<br>")
+
+var tarih = new Date()
+
+document.write(tarih.getDate()+"<br>")
+
+var tarih = new Date()
+
+document.write(tarih.getDay()+"<hr>")
